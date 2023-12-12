9912e78b02eeeff754d34e452e896a6340b273e5
udayanid <udhayan.addon@gmail.com>
2023-12-12 21:43:21 +0530

    removed unused imports

--- src/main/java/com/addon/productservice/dao/ProductRepository.java
+++ src/main/java/com/addon/productservice/dao/ProductRepository.java
@@ -6,8 +6,6 @@ import org.springframework.stereotype.Repository;
 
 import com.addon.productservice.entity.ProductEntity;
 
-import jakarta.transaction.Transactional;
-
 import java.util.List;
 import java.util.Optional;
 
